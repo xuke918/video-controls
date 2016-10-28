1be31d473ece5ec0b8a76006629a924faf412b7e
xuke918 <xuke3070@gmail.com>
2016-10-28 12:54:11 -0400

    Added a video control.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var $video = $('#the-video');
+var $btn = $('#btn-play-pause');
+
+var $videoWrap = $('#video-wrap');
+
+$btn.on('click', function () {
+  if ($video.get(0).paused) {
+    $video.get(0).play();
+    $btn.html('Pause')
+    $videoWrap.attr('data-state', 'playing');
+  } else {
+    $video.get(0).pause();
+    $btn.html('Play');
+    $videoWrap.attr('data-state', 'paused');
+  }
+});
